04edc5766854b23a79bfb964a90e691367fff4bc
zaintariq25 <64975638+zaintariq25@users.noreply.github.com>
2021-01-17 20:39:35 +0500

    Update index.php
    edit index

--- index.php
+++ index.php
@@ -13,10 +13,6 @@
 <p>
 Attempt to go to 
 <a href="game.php">game.php</a> without logging in - it should fail with an error message.
-<p>
-<a href="http://www.wa4e.com/code/rps.zip"
- target="_blank">Source Code for this Application</a>
-</p>
 </div>
 </body>
 
